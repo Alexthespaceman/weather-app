c7be25f2fc2109580fcc27e34be410d756b400ee
Alex Tristram <allex.tristram.gmail.com>
2021-06-01 22:21:02 +0100

    local weather app in global weather page

--- weather-app/src/Screens/LondonScreen.jsx
+++ weather-app/src/Screens/LondonScreen.jsx
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
 import * as api from "../api";
 import SideBar from "../components/SideBar";
+import IconFunction from "../functions/IconFunction";
+import ImageFuction from "../functions/ImageFuction";
+import weatherDescription from "../functions/tempFunction";
 
 class LondonScreen extends Component {
-  state = { weather: [] };
+  state = {
+    weather: [{ description: "Sun", temperature: "21", wind: "23 k/h" }],
+  };
 
   componentDidMount(city) {
     api.getLondonData(city).then((cityWeather) => {
@@ -13,14 +18,44 @@ class LondonScreen extends Component {
 
   getCityData = (city) => {
     api.getCityWeather(city).then((weather) => {
+      console.log(weather);
       return this.setState({ weather: weather });
     });
   };
 
   render() {
-    console.log(this.state.weather);
     return (
-      <div>
+      <div className="global-weather-screen">
+        <div className="weather-station">
+          <div className="font-loader-two">local weather in </div>
+          <div className="station">
+            {ImageFuction("rain")}
+            <div className="weather-icon">{IconFunction("rain")}</div>
+            <div className="primary-weather">
+              <div className="temp">{weatherDescription(24, 24)}</div>
+              <div className="wind">Wind Speed is {this.state.wind} </div>
+              <div className="description"> and {this.state.description}</div>
+            </div>
+
+            <div className="three-day-forecast">
+              <div className="forecast">
+                Tomorrow's forecast has highs of {this.state.temperature} and
+                wind speeds of
+                {this.state.wind}
+              </div>
+              {/* <div className="forecast">
+                {tomorrowsDate.day}
+                {day2temp}
+                {day2wind}
+              </div>
+              <div className="forecast">
+                {day3}
+                {day3temp}
+                {day3wind}
+              </div> */}
+            </div>
+          </div>
+        </div>
         <SideBar getCityData={this.getCityData} />
       </div>
     );
